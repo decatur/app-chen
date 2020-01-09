8b8c998aa10e97276633b6720a1a647b3697b333
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-01-09 14:03:25 +0100

    setup now works kindof

--- setup.py
+++ setup.py
@@ -2,31 +2,13 @@ from distutils.core import setup
 from setuptools.command.develop import develop
 
 # Stub needed to make pip install from git possible.
-import pathlib
-p = pathlib.Path(__file__).absolute()
-
-print(">>>>>>>>>" + str(p))
-
-class PostInstallCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        with ZipFile(p / 'client.zip', 'r') as zipObj:
-            # Extract all the contents of zip file in current directory
-            zipObj.extractall(path=p)
-
 
 setup(
     name='app-chen',
     version='0.1dev',
     packages=['appchen'],
-    package_data={'appchen': ['client.zip']},
-    include_package_data=True,
-    # include_package_data=True,
-    # data_files = [('appchen', 'appchen/client/app.js')],
+    include_package_data=True,  # This also needs a MANIFEST.in, and yes, python packaging sucks.
     license='MIT',
-    cmdclass={
-        #'install': PostInstallCommand
-    },
 )
 
 
