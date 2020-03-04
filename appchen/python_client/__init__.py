a6a65131f38289b8542d13638498c95bba8b8556
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-03-04 13:40:31 +0100

    removed unused code

--- appchen/python_client/__init__.py
+++ appchen/python_client/__init__.py
@@ -1,46 +1,54 @@
+"""A very small EventSource implementation based on the sseclient package."""
+
 import threading
-from typing import Dict, Callable
+from typing import List, Dict, Callable
 
 from sseclient import SSEClient, Event
 import time
 
 import requests
 
+EVENT_LISTENER = Callable[[Event], None]
+
 
 class EventSource:
     def __init__(self, url: str):
         self.url = url
-        self.event_listeners_by_type: Dict[str, Callable] = dict()
+        self._event_listeners_by_type: Dict[str, List[EVENT_LISTENER]] = dict()
 
-        def target():
+        def sse_connect():
             while True:
                 messages = SSEClient(self.url)
                 try:
                     for event in messages:
                         self._process_event(event)
-                except requests.exceptions.ConnectionError as e:
+                except (ConnectionResetError, requests.exceptions.ConnectionError) as e:
+                    # TODO: We do not need to handle ConnectionError, SSEClient does it for us.
+                    print(str(e))
                     pass
 
                 print('Reconnect...')
                 time.sleep(1)
 
-        threading.Thread(target=target).start()
+        threading.Thread(target=sse_connect).start()
 
     def _process_event(self, event: Event):
         print(event)
         if event.event == 'message':  # TODO: Spec says to call onmessage handler if event has no event field.
             self.onmessage(event)
-        elif event.event in self.event_listeners_by_type:
-            for callback in self.event_listeners_by_type[event.event]:
+        elif event.event in self._event_listeners_by_type:
+            for callback in self._event_listeners_by_type[event.event]:
                 callback(event)
 
     def onmessage(self, event: Event):
         pass
 
-    def add_event_listener(self, type: str, callback: Callable):
-        if type not in self.event_listeners_by_type:
-            self.event_listeners_by_type[type] = []
-        self.event_listeners_by_type[type].append(callback)
+    def add_event_listener(self, topic: str, callback: EVENT_LISTENER):
+        """Adds an event listener for a given topic.
+        """
+        if type not in self._event_listeners_by_type:
+            self._event_listeners_by_type[topic] = []
+        self._event_listeners_by_type[topic].append(callback)
 
     def route(self, topic: str):
         """A decorator that is used to add an event listener for a given topic.
@@ -49,49 +57,3 @@ class EventSource:
             self.add_event_listener(topic, f)
             return f
         return decorator
-
-
-# def connect():
-#     messages = SSEClient('http://localhost:8080/appchen/client/stream/connection')
-#     for evt in messages:
-#         print(evt.event)
-#
-#         if evt.event == 'connection_open':
-#             data: dict = json.loads(evt.data)
-#             print(data['connectionId'])
-#             r = requests.post('http://localhost:8080/appchen/client/stream/subscribe', data=json.dumps({
-#                 'connectionId': data['connectionId'],
-#                 'topics': ['zen']
-#             }))
-#             print(r.text)
-#         else:
-#             print(evt)
-
-
-# while True:
-#     try:
-#         connect()
-#     except requests.exceptions.ConnectionError as e:
-#         pass
-#
-#     print('Reconnect...')
-#     time.sleep(1)
-#
-
-
-# def my_handler(self, event: Event):
-#     print(event.event)
-#
-#     if event.event == 'connection_open':
-#         data: dict = json.loads(event.data)
-#         print(data['connectionId'])
-#         r = requests.post('http://localhost:8080/appchen/client/stream/subscribe', data=json.dumps({
-#             'connectionId': data['connectionId'],
-#             'topics': ['zen']
-#         }))
-#         print(r.text)
-#     else:
-#         print(event)
-# event_source.onmessage = partial(my_handler, event_source)
-
-
