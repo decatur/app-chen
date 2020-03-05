3d83d8846c3cb5690d97271d9eb90ca644e5fdc0
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-03-05 15:49:19 +0100

    pumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.develop import develop
 
 setup(
     name='app-chen',
-    version='0.1dev',
+    version='v0.0.5',
     packages=['appchen'],
     include_package_data=True,  # This also needs a MANIFEST.in, and yes, python packaging sucks.
     license='MIT',
